27cd0d1be662b5606e83f54787ed28640b01147e
lvan_software@foxmail.com <lvan_software@foxmail.com>
2013-07-31 18:06:01 +0800

    Android文件上传初始化

---
+++ uploadfile/src/com/example/uploadfile/MainActivity.java
@@ -0,0 +1,141 @@
+package com.example.uploadfile;
+
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+	
+	private String newName = "psbe.jpg";
+    private String uploadFile = "/sdcard/psbe.JPG";
+    private String actionUrl = "http://192.168.1.4:8086/hello";
+    private TextView mText1;
+    private TextView mText2;
+    private Button mButton;
+	
+	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		
+		
+		 /* 设置mButton的onClick事件处理 */    
+        mButton = (Button) findViewById(R.id.butUpload);
+        mButton.setOnClickListener(new View.OnClickListener()
+        {
+          public void onClick(View v)
+          {
+            uploadFile(actionUrl, uploadFile, newName);
+          }
+        });
+		
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	
+	 /* 上传文件至Server的方法 */
+    private void uploadFile(String actionUrl, String uploadFile, String fileName)
+    {
+      String end = "\r\n";
+      String twoHyphens = "--";
+      String boundary = "*****";
+      try
+      {
+        URL url =new URL(actionUrl);
+        HttpURLConnection con=(HttpURLConnection)url.openConnection();
+        /* 允许Input、Output，不使用Cache */
+        con.setDoInput(true);
+        con.setDoOutput(true);
+        con.setUseCaches(false);
+        /* 设置传送的method=POST */
+        con.setRequestMethod("POST");
+        /* setRequestProperty */
+        con.setRequestProperty("Connection", "Keep-Alive");
+        con.setRequestProperty("Charset", "UTF-8");
+        con.setRequestProperty("Content-Type",
+                           "multipart/form-data;boundary="+boundary);
+        /* 设置DataOutputStream */
+        DataOutputStream ds = 
+          new DataOutputStream(con.getOutputStream());
+        ds.writeBytes(twoHyphens + boundary + end);
+        ds.writeBytes("Content-Disposition: form-data; " +
+                      "name=\"userfile\";filename=\"" +
+                      fileName +"\"" + end);
+        ds.writeBytes(end);   
+
+        /* 取得文件的FileInputStream */
+        FileInputStream fStream = new FileInputStream(uploadFile);
+        /* 设置每次写入1024bytes */
+        int bufferSize = 1024;
+        byte[] buffer = new byte[bufferSize];
+
+        int length = -1;
+        /* 从文件读取数据至缓冲区 */
+        while((length = fStream.read(buffer)) != -1)
+        {
+          /* 将资料写入DataOutputStream中 */
+          ds.write(buffer, 0, length);
+        }
+        ds.writeBytes(end);
+        ds.writeBytes(twoHyphens + boundary + twoHyphens + end);
+
+        /* close streams */
+        fStream.close();
+        ds.flush();
+
+        /* 取得Response内容 */
+        InputStream is = con.getInputStream();
+        int ch;
+        StringBuffer b =new StringBuffer();
+        while( ( ch = is.read() ) != -1 )
+        {
+          b.append( (char)ch );
+        }
+        /* 将Response显示于Dialog */
+        showDialog("上传成功"+b.toString().trim());
+        /* 关闭DataOutputStream */
+        ds.close();
+      }
+      catch(Exception e)
+      {
+        showDialog("上传失败"+e);
+      }
+    }
+    
+    /* 显示Dialog的method */
+    private void showDialog(String mess)
+    {
+      new AlertDialog.Builder(MainActivity.this).setTitle("Message")
+       .setMessage(mess)
+       .setNegativeButton("确定",new DialogInterface.OnClickListener()
+       {
+         public void onClick(DialogInterface dialog, int which)
+         {          
+         }
+       })
+       .show();
+    }
+	
+	
+}
